5a513f43a78f1389b046ceee3150b8406d2068c8
Fábio Lima <fabioml@neemu.com>
2016-04-24 22:18:42 -0400

    Importing from other github

---
+++ utilities.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install htop
+sudo apt-get install terminator
+sudo apt-get install g++
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install gitk
+sudo apt-get install tig
